7e9660edaf711f4cfe6d1a8c1d0ac9728d90fd6e
hannahwallaker <hanwallaker@gmail.com>
2021-12-10 14:21:04 -0500

    Working Back-end updated!

--- SimCoinBack/src/main/resources/data.sql
+++ SimCoinBack/src/main/resources/data.sql
@@ -1,5 +1,5 @@
---insert into users(userid, username, password, email) values (1, 'test', 'test', 'test', 'test@test.com');
---
---insert into currency(currencyid, price, name) values (1, 50000, 'BTC');
---
---insert into currency_owned(userid, currencyid, amount) values (1, 1, 0.4);
+insert into users(userid, username, password, email) values (1, 'test', 'test', 'test', 'test@test.com');
+
+insert into currency(currencyid, price, name) values (1, 50000, 'BTC');
+
+insert into currency_owned(userid, currencyid, amount) values (1, 1, 0.4);
