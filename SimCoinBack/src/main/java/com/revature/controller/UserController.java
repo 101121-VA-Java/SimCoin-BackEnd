a2a9c5456239fa5b13ede3e6762eda2c7fe2eb57
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2021-12-13 17:52:03 -0700

    trying to get a response from the controller for user-stats

--- SimCoinBack/src/main/java/com/revature/controller/UserController.java
+++ SimCoinBack/src/main/java/com/revature/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.revature.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.model.Currency;
 import com.revature.model.User;
 import com.revature.services.UserService;
 
@@ -30,4 +35,19 @@ public class UserController {
 		us.addUser(user);
 		return new ResponseEntity<>(user.getUserid() + " was created.", HttpStatus.CREATED);
 	}
+	@RequestMapping(method=RequestMethod.GET) public User getCurrentUser(){
+		User u = new User();
+		
+		u.setUserid(1);
+		u.setUsername("test username");
+		u.setEmail("testuser@testing.com");
+		u.setPassword("testpass");
+		/*
+		 * u.setTotal(75000); u.setCash(25000); u.setNetGain(25000); u.setBtc(30000);
+		 * u.setEth(1500); u.setLtc(10000); u.setXmr(0); u.setTrx(0);
+		 */
+		
+		return u;
+	}
+	
 }
